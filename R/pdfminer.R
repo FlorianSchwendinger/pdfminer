4f3749aafe93b03ea9f183c7ad51307257d118da
Florian Schwendinger <Florian_Schwendinger@gmx.at>
2020-04-20 21:45:34 +0200

    first commit

---
+++ R/pdfminer.R
@@ -0,0 +1,198 @@
+#' @importFrom checkmate assert check_file_exists check_directory_exists check_integerish check_character check_logical
+#' @importFrom jsonlite toJSON
+#' @importFrom utils read.csv
+
+#  ---------------------------------------------------------
+#  layout_control
+#  ==============
+#' @title Read a \code{PDF} document.
+#' @description Extract  \code{PDF} document
+#' @param line_overlap a double, if two characters have more overlap than this 
+#'     they are considered to be on the same line. The overlap is specified
+#'     relative to the minimum height of both characters.
+#' @param char_margin a double, if two characters are closer together than this
+#'     margin they are considered part of the same line. The margin is
+#'     specified relative to the width of the character.
+#' @param line_margin a double, if two characters on the same line are further 
+#'     apart than this margin then they are considered to be two separate words, 
+#'     and an intermediate space will be added for readability. The margin is
+#'     specified relative to the width of the character.
+#' @param word_margin a double, if two lines are are close together they are
+#'     considered to be part of the same paragraph. The margin is specified 
+#'     relative to the height of a line.
+#' @param boxes_flow a double, Specifies how much a horizontal and vertical 
+#'     position of a text matters when determining the order of text boxes. 
+#'     The value should be within the range of \code{-1.0} (only horizontal 
+#'     position matters) to \code{+1.0} (only vertical position matters). 
+#'     You can also pass \code{NULL} to disable advanced layout analysis, 
+#'     and instead return text based on the position of the bottom left corner 
+#'     of the text box.
+#' @param detect_vertical a logical, If vertical text should be considered during
+#'     layout analysis
+#' @param all_texts a logical, If layout analysis should be performed on text 
+#'     in figures.
+#' @return Returns a list with the layout control variables.
+#' @export
+##  ---------------------------------------------------------
+layout_control <- function(line_overlap = 0.5, char_margin = 2.0, line_margin = 0.5,
+    word_margin = 0.1, boxes_flow = 0.5, detect_vertical = FALSE, all_texts = FALSE) {
+    cntrl <- as.list(environment())
+    return(cntrl)
+}
+
+
+read_sqlite_tables <- function(output_file) {
+    suppressPackageStartupMessages(is_installed <- requireNamespace("RSQLite"))
+    if (!is_installed) {
+        stop("there is no package called 'RSQLite'")
+    }
+    conn <- RSQLite::dbConnect(RSQLite::SQLite(), output_file)
+    tables <- RSQLite::dbListTables(conn)
+    queries <- sprintf("SELECT * FROM %s;", tables)
+
+    get_table <- function(query, conn) {
+        d <- RSQLite::dbGetQuery(conn, query)
+        d$index <- NULL
+        d
+    }
+    dat <- lapply(queries, get_table, conn = conn)
+    names(dat) <- tables
+    return(dat)
+}
+
+
+as_integer <- function(x) {
+    x <- as.integer(x)
+    x[!is.finite(x)] <- NA_integer_
+    x
+}
+
+
+as_double <- function(x) {
+    x <- as.double(x)
+    x[!is.finite(x)] <- NA_real_
+    x
+}
+
+
+fix_dtype <- function(x, dtype) {
+    if (dtype == "integer") {
+        as_integer(x)
+    } else if (dtype == "double") {
+        as_double(x)
+    } else if (dtype == "character") {
+        as.character(x)
+    }
+}
+
+
+to_data_frame <- function(x) {
+    d <- mapply(fix_dtype, x[['data']], x[['dtypes']], SIMPLIFY = FALSE)
+    names(d) <- x[['colnames']]
+    as.data.frame(d, stringsAsFactors = FALSE)
+}
+
+
+#  ---------------------------------------------------------
+#  read.pdf
+#  ========
+#' @title Read a \code{PDF} document.
+#' @description Extract  \code{PDF} document
+#' @param file a character giving the name of the \code{PDF}-file the data are 
+#'     to be read from.
+#' @param pages an integer giving the pages which should be extracted 
+#'     (default is \code{integer()}).
+#' @param laycntrl a list of layout options, created by the function \code{layout_control}.
+#' @param method a character giving the data transfer method. Allowed values
+#'     are \code{"csv"} (default), \code{"sqlite"} and \code{"PythonInR"} (recommended).
+#' @param encoding a character string giving the encoding of the output 
+#'     (default is \code{"utf8"}).
+#' @param strip_control a logical, if \code{TRUE} control characters are removed.
+#' @param password a character string giving the password necessary to access 
+#'     the \code{PDF} (default is \code{""}).
+#' @param caching a logical if \code{TRUE} (default) \pkg{pdfminer} is faster but 
+#'     uses more memory.
+#' @param maxpages an integer giving the maximum number of pages to be extracted
+#'     (default is \code{Inf}).
+#' @param rotation an integer giving the rotation of the page, allowed values
+#'     are \code{c(0, 90, 180, 270)}.
+#' @param image_dir a character giving the path to the folder, where the images
+#'     should be stored (default is \code{""}).
+#' @param pyexe a character giving the path to the python executeable 
+#'     (default is \code{"python3"}). Only used when \code{method} is 
+#'     \code{"csv"} or \code{"sqlite"}.
+#' @examples
+#' \dontrun{
+#' ifi <- system.file("pdfs/cars.pdf", package = "pdfmole")
+#' read.pdf(ifi)
+#' }
+#' @return Returns a object of class \code{"pdf_document"}.
+#'         A object of class \code{"pdf_document"} is a list where
+#'         each element of the list is an object of class \code{"pdf_page"}.
+#' @export
+##  ---------------------------------------------------------
+read.pdf <- function(file, pages = integer(), method = c('csv', 'sqlite', 'PythonInR'),
+    laycntrl = layout_control(), encoding = 'utf8', strip_control = FALSE, 
+    password = '', caching = TRUE, maxpages = Inf, rotation = 0L, image_dir = '',
+    pyexe='python3') {
+
+    cntrl <- as.list(environment())
+    cntrl <- cntrl[!names(cntrl) %in% c('method', 'pyexe')]
+
+    method <- match.arg(method)
+    if (is.infinite(maxpages)) cntrl$maxpages <- 0L
+
+    assert(check_file_exists(file), check_integerish(pages), 
+           check_character(encoding), check_logical(strip_control),
+           check_character(password), check_logical(caching),
+           check_integerish(maxpages), check_integerish(rotation),
+           check_character(image_dir))
+
+
+    if (!rotation %in% (seq.int(0, 3) * 90L)) {
+        stop("invalid rotation value! Allowed rotation values are c(0, 90, 180, 270).")
+    }
+
+    pdf_elements <- c('metainfo', 'text', 'line', 'rect', 'curve', 'figure', 
+        'textline', 'textbox', 'textgroup', 'image')
+
+    rpdfmine_file <- system.file("python/rpdfmine.py", package = "pdfminer")
+    cntrl$pages <- as.list(pages - 1L)
+    names(cntrl) <- gsub("encoding", "codec", names(cntrl), fixed = TRUE)
+
+    if (method %in% c("csv", "sqlite")) {    
+        if (method == "csv") {
+            output_file <- tempfile()
+            file_names <- sprintf("%s_%s.csv", output_file, pdf_elements)
+            on.exit(unlink(file_names))
+        } else {
+            output_file <- sprintf("%s.sqlite", tempfile())
+            on.exit(unlink(output_file))
+        }
+        json_cntrl <- shQuote(toJSON(cntrl, auto_unbox = TRUE))
+        cmd <- sprintf("%s %s %s %s", rpdfmine_file, method, output_file, json_cntrl)
+        suppressWarnings(msg <- system2(pyexe, cmd, stdout=TRUE, stderr=TRUE))
+        if (!is.null(attr(msg, "status"))) stop(paste(msg, collapse = "\n"))
+        if (method == "csv") {
+            dat <- lapply(file_names, read.csv, stringsAsFactors = FALSE)
+            names(dat) <- gsub("(.*_|.csv)", "", file_names)
+        } else {
+            dat <- read_sqlite_tables(output_file)
+        }
+    } else if (method == "PythonInR") {
+        suppressPackageStartupMessages(is_installed <- requireNamespace("PythonInR"))
+        if (!is_installed) {
+            stop("there is no package called 'PythonInR'")
+        }
+        script_folder <- system.file('python', package = 'pdfminer') 
+        PythonInR::pyExec(sprintf('sys.path.append("%s")', script_folder))
+        PythonInR::pyExec('import rpdfmine as rpdf')
+        PythonInR::pySet('cntrl', cntrl)
+        PythonInR::pyExec('x = rpdf.read_pdf(**cntrl).to_dict(dtype="data.frame")')
+        dat <- PythonInR::pyGet("x")
+        dat <- lapply(dat, to_data_frame)
+    }
+    dat <- dat[pdf_elements]
+    class(dat) <- "pdf_document"
+    return(dat)
+}
---
+++ README.md
@@ -0,0 +1,87 @@
+# **pdfminer**
+
+The **R** package **pdfminer** provides an interface to low level functionality
+of the **Python** package **pdfminer**.
+
+## Installation
+### **Python**
+```{shell}
+pip install pdfminer.six
+pip install pandas
+```
+
+### **R**
+```{r}
+install.packages("pdfminer")
+```
+
+## Basic usage
+```{r}
+library("pdfminer")
+
+args(read.pdf)
+#R> function (file, pages = integer(), method = c("csv", "sqlite", 
+#R>     "PythonInR"), laycntrl = layout_control(), encoding = "utf8", 
+#R>     strip_control = FALSE, password = "", caching = TRUE, maxpages = Inf, 
+#R>     rotation = 0L, image_dir = "", pyexe = "python3") 
+```
+
+```{r}
+file <- system.file("pdfs/cars.pdf", package = "pdfminer")
+d <- read.pdf(file, method = "csv")
+#R> A pdf document with 2 pages and
+#R>   metainfo text line rect curve figure textline textbox textgroup image
+#R> 1        2  469    0    0     0      0      155      10         8     0
+#R> elements.
+```
+The function `read.pdf()` returns an object of class `pdf_document`
+(a list containing `data.frame`'s). Each object of class `pdf_document`
+contains the elements:
+
+- `"metainfo"`
+- `"text"`
+- `"line"`
+- `"rect"`
+- `"curve"`
+- `"figure"`
+- `"textline"`
+- `"textbox"`
+- `"textgroup"`
+- `"image"`
+
+The elements can be accessed as by each other list.
+```{r}
+head(d[["text"]])
+#R>   pid block text         font size colorspace     color    x0      y0    x1      y1
+#R> 1   1     1    s Courier-Bold   12 DeviceGray [0, 0, 0]  77.2 751.272  84.4 763.272
+#R> 2   1     1    p Courier-Bold   12 DeviceGray [0, 0, 0]  84.4 751.272  91.6 763.272
+#R> 3   1     1    e Courier-Bold   12 DeviceGray [0, 0, 0]  91.6 751.272  98.8 763.272
+#R> 4   1     1    e Courier-Bold   12 DeviceGray [0, 0, 0]  98.8 751.272 106.0 763.272
+#R> 5   1     1    d Courier-Bold   12 DeviceGray [0, 0, 0] 106.0 751.272 113.2 763.272
+#R> 6   1    NA                     NA                         NA      NA    NA      NA
+``` 
+
+The **R** package **pdfminer** only returns raw data extracted from the
+**PDF**-file. To refine this raw data into a format usable for data analysis
+the [**pdfmole**](https://CRAN.R-project.org/package=pdfmole) can be used.
+ 
+
+### Details on the data exchange
+The data exchange between **Python** and **R** can be executed by 
+one of the methods `"csv"`, `"sqlite"` or `"PythonInR"`.
+The methods `"csv"` and `"sqlite"` call **Python** via the `system2`
+command and the data is written out to temporary files.
+The **Python** version called by `system2` can be changes by changing the
+`pyexe` argument. For example if a specific conda environment (in this
+example the `pdf` environment) should be used. Obtain the path to
+the **Python** executable
+```{python}
+import sys
+sys.executable
+Py> '/home/f/anaconda3/envs/pdf/bin/python'
+```
+and specify it via the `pyexe` argument.
+```{r}
+pyexe <- '/home/f/anaconda3/envs/pdf/bin/python'
+d <- read.pdf(file, method = "sqlite", pyexe=pyexe)
+```
